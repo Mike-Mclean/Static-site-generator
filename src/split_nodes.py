4b1b620d93d209dcff5b642ba3c835edb236834c
Mike-Mclean <mmcle009@gmail.com>
2026-02-10 17:54:48 -0500

    Added text_to_textnodes function and tests

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -20,6 +20,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 def split_nodes_image(old_nodes):
     new_nodes = []
     for node in old_nodes:
+        if not node.text_type is TextType.TEXT:
+            new_nodes.append(node)
+            continue
         remaining_text = node.text
         images = extract_markdown_images(remaining_text)
         for image in images:
@@ -31,13 +34,16 @@ def split_nodes_image(old_nodes):
                 TextNode(alt, TextType.IMAGES, url),
             ])
         if remaining_text:
-            new_nodes.append(TextNode(remaining_text), TextType.TEXT)
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
 
 
 def split_nodes_link(old_nodes):
     new_nodes = []
     for node in old_nodes:
+        if not node.text_type is TextType.TEXT:
+            new_nodes.append(node)
+            continue
         remaining_text = node.text
         links = extract_markdown_links(remaining_text)
         for link in links:
@@ -49,5 +55,28 @@ def split_nodes_link(old_nodes):
                 TextNode(alt, TextType.LINKS, url),
             ])
         if remaining_text:
-            new_nodes.append(TextNode(remaining_text), TextType.TEXT)
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
+
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    nodes_to_process = [node]
+    delimiters = {"**": TextType.BOLD, "_": TextType.ITALIC, "`": TextType.CODE}
+    for delimiter in delimiters:
+        new_nodes = split_nodes_delimiter(nodes_to_process, delimiter, delimiters[delimiter])
+        nodes_to_process = new_nodes
+
+    new_nodes = split_nodes_image(nodes_to_process)
+    nodes_to_process = new_nodes
+    new_nodes = split_nodes_link(nodes_to_process)
+
+    return new_nodes
+
+
+if __name__ == "__main__":
+    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+    nodes = text_to_textnodes(text)
+
+    for node in nodes:
+        print(node)
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -156,5 +156,22 @@ class TestTextNode(unittest.TestCase):
             new_nodes
         )
 
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://www.google.com)"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGES, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINKS, "https://www.google.com")
+        ]
+        self.assertListEqual(nodes, expected_nodes)
+
 if __name__ == "__main__":
     unittest.main()
