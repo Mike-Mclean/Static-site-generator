36615dec47c31bc011cfb3d9e0ee9abca592d460
Mike-Mclean <mmcle009@gmail.com>
2026-02-10 11:59:58 -0500

    Added split_nodes_image and split_nodes_link functions

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+from extract_from_mds import *
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -14,4 +15,39 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(node_sections[index], TextType.TEXT))
             elif index % 2 != 0:
                 new_nodes.append(TextNode(node_sections[index], text_type))
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        remaining_text = node.text
+        images = extract_markdown_images(remaining_text)
+        for image in images:
+            alt = image[0]
+            url = image[1]
+            before, remaining_text = remaining_text.split(f"![{alt}]({url})", 1)
+            new_nodes.extend([
+                TextNode(before, TextType.TEXT),
+                TextNode(alt, TextType.IMAGES, url),
+            ])
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text), TextType.TEXT)
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        remaining_text = node.text
+        links = extract_markdown_links(remaining_text)
+        for link in links:
+            alt = link[0]
+            url = link[1]
+            before, remaining_text = remaining_text.split(f"[{alt}]({url})", 1)
+            new_nodes.extend([
+                TextNode(before, TextType.TEXT),
+                TextNode(alt, TextType.LINKS, url),
+            ])
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text), TextType.TEXT)
     return new_nodes
