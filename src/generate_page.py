4102350435b47f54711b860781c7652151d93bc3
Mike-Mclean <mmcle009@gmail.com>
2026-03-13 09:29:18 -0400

    - Created index.html with basic structure and content - Added build script to deploy site using Github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,7 +1,7 @@
 from split_blocks import *
 import os
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
 
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
@@ -16,6 +16,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/"', f'href="{basepath}')
+    template = template.replace('src="/"', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -23,3 +25,13 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(dest_path, "w") as new_html:
         new_html.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for item in os.listdir(dir_path_content):
+        item_path = os.path.join(dir_path_content, item)
+        if not os.path.isfile(item_path):
+            new_dest_path = os.path.join(dest_dir_path, item)
+            generate_pages_recursive(item_path, template_path, new_dest_path, basepath)
+        else:
+            new_dest_path = os.path.join(dest_dir_path, "index.html")
+            generate_page(item_path, template_path, new_dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
-import os
 from generate_page import *
 from copystatic import dir_copy
+import sys
 
 static_path = "./static"
-public_path = "./public"
+public_path = "./docs"
 content_path = "./content"
 template_path = "./template.html"
 
 def main():
+
+    if sys.argv[1] is None:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
+
     dir_copy(static_path, public_path)
-
-    from_path = os.path.join(content_path, "index.md")
-    dest_path = os.path.join(public_path, "index.html")
-    generate_page(from_path, template_path, dest_path)
-
+    generate_pages_recursive(content_path, template_path, public_path, basepath)
 
 if __name__ == "__main__":
     main()
